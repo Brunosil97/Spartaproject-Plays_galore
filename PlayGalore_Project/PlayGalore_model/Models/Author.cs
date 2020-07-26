371593984d9042f882f9402489ed41d1bda9139b
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-26 16:53:12 +0100

    added models folder to have database connection and have classes for each table

---
+++ PlayGalore_Project/PlayGalore_model/Models/Author.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayGalore_model.Models
+{
+    public partial class Author
+    {
+    }
+}
---
+++ PlayGalore_Project/PlayGalore_model/Models/DbConnection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlayGalore_model.Models
+{
+    class DbConnection
+    {
+    }
+}
