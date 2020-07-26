5585a62391836197013604a0301cd192587132b2
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-26 16:39:42 +0100

    Added the three projects into the solution

---
+++ PlayGalore_Project/PlayGalore_controller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayGalore_controller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
